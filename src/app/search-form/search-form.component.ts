8a458f9ec89db5d112b45f7f6d6b12864c7cccbf
Ivan Videnovic <outlaw_serni@yahoo.com>
2017-12-20 23:08:24 +0100

    Reactive search functionality - basics

--- src/app/search-form/search-form.component.ts
+++ src/app/search-form/search-form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
+import 'rxjs/Rx';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-search-form',
@@ -7,15 +9,53 @@ import { FormGroup, FormControl } from '@angular/forms';
   styleUrls: ['./search-form.component.css']
 })
 export class SearchFormComponent implements OnInit {
+  employees = [
+      { name: 'John Doe', age: 45 },
+      { name: 'Michael Evans', age: 25 },
+      { name: 'Anna Johnson', age: 31 },
+      { name: 'Chris Donovan', age: 50 },
+      { name: 'Philip Anderson', age: 52 },
+      { name: 'Andrew Pierce', age: 41 },
+      { name: 'Paul Ackermann', age: 33 }
+  ];
+
+  filteredEmployees = [];
 
   searchForm: FormGroup;
 
-  constructor() { }
+  constructor() {
+    this.filteredEmployees = this.employees;
+  }
 
   ngOnInit() {
     this.searchForm = new FormGroup({
         'search': new FormControl()
     });
+
+    this.searchForm.get('search').valueChanges
+        .subscribe(
+            value => {
+              this.filteredEmployees = this.filterEmployees(value);
+            }
+        );
+  }
+
+
+  filterEmployees(value) {
+
+    let found = [];
+
+      Observable.from(this.employees)
+        .filter(
+            person => person.name.toLowerCase().search(value.toLowerCase()) !== -1
+        )
+        .subscribe(
+            emp => {
+                found.push(emp);
+            }
+        );
+
+      return found;
   }
 
 }
