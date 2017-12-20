667f049710401bbc94a11e02d9786b18b1d4e9d7
Ivan Videnovic <outlaw_serni@yahoo.com>
2017-12-20 20:02:45 +0100

    Reactive Search Form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { SearchFormComponent } from './search-form/search-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+      ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
